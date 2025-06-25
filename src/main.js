881342ada02450defc04dabafb9520bdc7f3b330
Mateo Mayorga <103150863+bironmanusa@users.noreply.github.com>
2025-06-25 19:44:15 +0000

    Taller 2

--- src/main.js
+++ src/main.js
@@ -23,3 +23,65 @@ const itemData = {
         score: 36
     }
 };
+
+let valor = 0;
+
+const agregarNombres= () =>{
+    const selector = document.getElementById("items");
+    if (selector){
+        Object.values(itemData).forEach(item => {
+            const option = document.createElement("option");
+            option.value = valor++;
+            option.text = item.name
+            selector.add(option);
+        });
+    }
+}
+
+const mostrarDatos = () => {
+    const imagen = document.getElementById("displayImage");
+    const photographer = document.getElementById("photographer");
+    const description = document.getElementById("description");
+    const score = document.getElementById("score");
+    const selector = document.getElementById("items");
+    const selectedValue = selector.value;
+    const selectedItem = Object.values(itemData)[selectedValue];
+
+    if (selectedItem) {
+        imagen.src = selectedItem.image;
+        photographer.value = selectedItem.photographer;
+        description.value = selectedItem.description;
+        score.value = selectedItem.score;
+        selector.addEventListener("change", mostrarDatos);
+    
+    }
+}
+
+const modificarPuntaje = () => {
+    const selector = document.getElementById("items");
+    const botonAumentar = document.getElementById("increaseScore");
+    const botonDisminuir = document.getElementById("decreaseScore");
+
+    const actualizarPuntaje = (delta) => {
+        const selectedValue = selector.value;
+        const selectedItem = Object.values(itemData)[selectedValue];
+        if (selectedItem) {
+            selectedItem.score += delta;
+            const score = document.getElementById("score");
+            score.value = selectedItem.score;
+        }
+    };
+
+    if (botonAumentar) {
+        botonAumentar.addEventListener("click", () => actualizarPuntaje(1));
+    }
+    if (botonDisminuir) {
+        botonDisminuir.addEventListener("click", () => actualizarPuntaje(-1));
+    }
+}
+
+(() => {
+    agregarNombres();
+    mostrarDatos();
+    modificarPuntaje();
+})();
